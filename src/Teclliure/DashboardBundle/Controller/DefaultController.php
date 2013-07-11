e7f0636f67742f288aba95c61f16704244a499e7
Marc Montañés <marc@teclliure.net>
2013-07-11 16:53:33 +0200

    * Redirect dashboard to inovice list

--- src/Teclliure/DashboardBundle/Controller/DefaultController.php
+++ src/Teclliure/DashboardBundle/Controller/DefaultController.php
@@ -8,6 +8,8 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $this->getRequest()->setLocale($this->getRequest()->getPreferredLanguage(array('en', 'ca', 'es' )));
+        return $this->redirect($this->generateUrl('invoice_list'));
         return $this->render('TeclliureDashboardBundle:Default:index.html.twig');
     }
 }
